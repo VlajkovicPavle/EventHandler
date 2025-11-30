4046d5d3eedc93048160001c4bdf8eddb8868d24
VlajkovicPavle <vlajkovicpavle123@gmail.com>
2025-11-30 13:03:28 +0100

    feat(bus): unsubscribe logic

--- core/event-bus.ts
+++ core/event-bus.ts
@@ -13,6 +13,27 @@ export class EventBus<TEventMap extends Record<string, any>> {
     this._handlers.get(event)!.push(handler);
   }
 
+  unsubscribe<K extends keyof TEventMap>(
+    event: K,
+    handler: EventHandler<TEventMap, K>,
+  ) {
+    const handlers = this._handlers.get(event);
+    if (!handlers) {
+      throw new Error("[EventBus] - Event not found");
+    }
+    const filtered = handlers.filter(
+      (savedHandler) => savedHandler !== handler,
+    );
+    if (filtered.length === handlers.length) {
+      throw new Error("[EventBus] - Handler not found");
+    }
+    this._handlers.set(event, filtered);
+  }
+
+  unsubscribeALl<K extends keyof TEventMap>(event: K) {
+    return this._handlers.delete(event);
+  }
+
   publish<K extends keyof TEventMap>(event: K, data: TEventMap[K]) {
     this._handlers.get(event)?.forEach((handler) => {
       handler(data);
